3dbc0a06e591a9ebc381b3886c68867bf49420bb
Brad Starkenberg <brad@starkenberg.net>
2023-04-12 12:52:41 -0500

    add form handling

---
+++ src/main/java/edu/uark/csce/databasehb/web/StudentForm.java
@@ -0,0 +1,31 @@
+package edu.uark.csce.databasehb.web;
+
+public class StudentForm {
+    private String studentId;
+    private String studentName;
+    private String majorId;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getMajorId() {
+        return majorId;
+    }
+
+    public void setMajorId(String majorId) {
+        this.majorId = majorId;
+    }
+}
--- src/main/java/edu/uark/csce/databasehb/web/WebController.java
+++ src/main/java/edu/uark/csce/databasehb/web/WebController.java
@@ -4,6 +4,8 @@ import edu.uark.csce.databasehb.data.StudentRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -53,4 +55,9 @@ public class WebController {
         return "index";
     }
 
+    @PostMapping("/addStudent")
+    public String addStudent(@RequestBody StudentForm form) {
+        System.out.println(form);
+        return "add_student";
+    }
 }
