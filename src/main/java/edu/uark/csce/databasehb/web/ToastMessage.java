fde254540ee494cedf25dcd72b95bae8baf01baa
Bailey Grimes <baileyjgrimes@gmail.com>
2023-04-13 20:17:48 -0500

    Add symbols to addStudent toasts

--- src/main/java/edu/uark/csce/databasehb/web/ToastMessage.java
+++ src/main/java/edu/uark/csce/databasehb/web/ToastMessage.java
@@ -2,13 +2,17 @@ package edu.uark.csce.databasehb.web;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class ToastMessage {
-    private static final String baseClass = " alert-dismissible fade show";
-    private String cssClass = "alert alert-success alert-dismissible fade show";
+    private static final String baseClass = " d-flex align-items-center alert-dismissible fade show";
+    private String cssClass = "alert alert-success d-flex align-items-center alert-dismissible fade show";
     private String message = "Everything worked as expected";
+    private String symbol = "#check-circle-fill";
 
     public void setCssClass(String baseCssAlert) {
         cssClass = baseCssAlert + baseClass;
+        symbol = "#exclamation-triangle-fill";
     }
 }
