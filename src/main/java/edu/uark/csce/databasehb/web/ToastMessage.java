bb5b64bb6b5522512f0292f8a7b87bf4a2809878
Brad Starkenberg <brad@starkenberg.net>
2023-04-17 16:10:18 -0500

    refactoring of pages

--- src/main/java/edu/uark/csce/databasehb/web/ToastMessage.java
+++ src/main/java/edu/uark/csce/databasehb/web/ToastMessage.java
@@ -1,18 +1,22 @@
 package edu.uark.csce.databasehb.web;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ToastMessage {
-    private static final String baseClass = " d-flex align-items-center alert-dismissible fade show";
-    private String cssClass = "alert alert-success d-flex align-items-center alert-dismissible fade show";
+    private static final String baseClass = " alert d-flex align-items-center alert-dismissible fade show";
+    private String cssClass = "alert-success" + baseClass;
     private String message = "Everything worked as expected";
-    private String symbol = "#check-circle-fill";
+    private String symbol = "fa-check-circle";
 
-    public void setCssClass(String baseCssAlert) {
+    public void setCssClass(String baseCssAlert, String symbolClass) {
         cssClass = baseCssAlert + baseClass;
-        symbol = "#exclamation-triangle-fill";
+        symbol = symbolClass;
     }
 }
--- src/main/java/edu/uark/csce/databasehb/web/WebController.java
+++ src/main/java/edu/uark/csce/databasehb/web/WebController.java
@@ -41,16 +41,16 @@ public class WebController {
             try {
                 boolean duplicateValue = studentRepo.addStudent(form);
                 if (duplicateValue) {
-                    toast.setCssClass("alert alert-warning");
+                    toast.setCssClass("alert-warning", "fa-question-circle");
                     toast.setMessage("Student already exists in the database, updating values instead");
                 } else
                     toast.setMessage(form.getStudentName() + " has been added to the database");
             } catch (Exception e) {
-                toast.setCssClass("alert alert-danger");
+                toast.setCssClass("alert-danger", "fa-exclamation-triangle");
                 toast.setMessage(e.getMessage());
             }
         } else {
-            toast.setCssClass("alert alert-warning");
+            toast.setCssClass("alert-warning", "fa-question-circle");
             toast.setMessage("Invalid Value(s) in form");
         }
         log.info("Toast: {}", toast);
@@ -76,18 +76,18 @@ public class WebController {
             try {
                 boolean duplicateValue = jobRepo.addJob(form);
                 if (duplicateValue) {
-                    toast.setCssClass("alert alert-warning");
+                    toast.setCssClass("alert-warning", "fa-question-circle");
                     toast.setMessage("Job already exists in the database");
                 } else {
                     toast.setMessage(form.getCompanyName() + " has been added to the database");
                 }
                 jobRepo.addJobToJobMajor(form);
             } catch (Exception e) {
-                toast.setCssClass("alert alert-danger");
+                toast.setCssClass("alert-danger", "fa-exclamation-triangle");
                 toast.setMessage(e.getMessage());
             }
         } else {
-            toast.setCssClass("alert alert-warning");
+            toast.setCssClass("alert-warning", "fa-question-circle");
             toast.setMessage("Invalid Value(s) in form");
         }
         log.info("Toast: {}", toast);
@@ -112,15 +112,15 @@ public class WebController {
                 boolean duplicateValue = applicationRepo.addApplication(form);
                 if (duplicateValue) {
                     toast.setMessage("Application already exists in the database");
-                    toast.setCssClass("alert alert-warning");
+                    toast.setCssClass("alert-warning", "fa-question-circle");
                 } else
                     toast.setMessage("Application has been added to the database");
             } catch (Exception e) {
-                toast.setCssClass("alert alert-danger");
+                toast.setCssClass("alert-danger", "fa-exclamation-triangle");
                 toast.setMessage(e.getMessage());
             }
         } else {
-            toast.setCssClass("alert alert-warning");
+            toast.setCssClass("alert-warning", "fa-question-circle");
             toast.setMessage("Invalid Value(s) in form");
         }
         model.addAttribute("toast", toast);
@@ -131,10 +131,8 @@ public class WebController {
     @GetMapping("/viewStudent")
     public String viewStudent(Model model) {
         log.info("ViewStudents GET");
-        List<Major> majors = majorRepository.getAllMajors();
-        Boolean initialLoad = true;
-        model.addAttribute("majors", majors);
-        model.addAttribute("initialLoad", initialLoad);
+        model.addAttribute("majors", majorRepository.getAllMajors());
+        model.addAttribute("initialLoad", true);
         model.addAttribute("viewName", "view_students");
         return "view_students";
     }
@@ -146,10 +144,8 @@ public class WebController {
         majors.add(0, majors.stream().filter(s -> s.getMajorId() == major).toList().get(0));
         log.info("MajorList: {}", majors);
         model.addAttribute("majors", majors);
-        List<Student> studentList = studentRepo.getStudentsByMajor(major);
-        Boolean initialLoad = false;
-        model.addAttribute("students", studentList);
-        model.addAttribute("initialLoad", initialLoad);
+        model.addAttribute("students", studentRepo.getStudentsByMajor(major));
+        model.addAttribute("initialLoad", false);
         model.addAttribute("viewName", "view_students");
         return "view_students";
     }
@@ -157,33 +153,28 @@ public class WebController {
     @GetMapping("/viewJob")
     public String viewJob(Model model) {
         log.info("ViewJobs GET");
-        List<Major> majors = majorRepository.getAllMajors();
-        Boolean initialLoad = true;
-        model.addAttribute("majors", majors);
-        model.addAttribute("initialLoad", initialLoad);
+        model.addAttribute("majors", majorRepository.getAllMajors());
+        model.addAttribute("initialLoad", true);
         model.addAttribute("viewName", "view_jobs");
         return "view_jobs";
     }
 
     @PostMapping("/viewJob")
     public String viewJob(@RequestParam("major") Integer major, Model model) {
-        log.info("ViewJobs POST");
+        log.trace("ViewJobs POST");
         List<Major> majors = majorRepository.getAllMajors();
         majors.add(0, majors.stream().filter(s -> s.getMajorId() == major).toList().get(0));
-        log.info("MajorList: {}", majors);
+        log.debug("MajorList: {}", majors);
         model.addAttribute("majors", majors);
-        List<Job> jobList = jobRepo.getJobByMajor(major);
-        Boolean initialLoad = false;
-        model.addAttribute("jobs", jobList);
-        model.addAttribute("initialLoad", initialLoad);
+        model.addAttribute("jobs", jobRepo.getJobByMajor(major));
+        model.addAttribute("initialLoad", false);
         model.addAttribute("viewName", "view_jobs");
         return "view_jobs";
     }
 
     @GetMapping("/viewApplication")
     public String viewApplication(Model model) {
-        boolean noList = true;
-        model.addAttribute("noList", noList);
+        model.addAttribute("noList", true);
         model.addAttribute("searchMethod", 0);
         model.addAttribute("viewName", "view_applications");
         return "view_applications";
@@ -193,33 +184,26 @@ public class WebController {
     public String viewApplication(@RequestParam("searchMethod") Integer searchMethod, Model model) {
         log.info("***** viewApplication *****");
         boolean noList = true;
-        model.addAttribute("searchMethod", searchMethod);
         switch (searchMethod) {
             case 1 -> { // View all applications
                 List<ViewApplicationForm> applications = applicationRepo.getAllApplications();
                 model.addAttribute("allApplications", applications);
                 noList = false;
-                model.addAttribute("noList", noList);
             }
             case 2 -> { // View all applications by specified majorId
                 List<Major> majors = majorRepository.getAllMajors();
                 model.addAttribute("majorList", majors);
-                model.addAttribute("noList", noList);
             }
             case 3 -> { // View all applications by specified studentId
                 List<Student> students = studentRepo.getAllStudents();
                 model.addAttribute("studentList", students);
-                model.addAttribute("noList", noList);
             }
             case 4 -> { // View all applications by specified jobId
                 List<Job> jobs = jobRepo.getAllJobs();
                 model.addAttribute("jobList", jobs);
-                model.addAttribute("noList", noList);
-            }
-            default -> {
-                model.addAttribute("noList", noList);
             }
         }
+        model.addAttribute("noList", noList);
         model.addAttribute("searchMethod", searchMethod);
         model.addAttribute("viewName", "view_applications");
         return "view_applications";
@@ -231,35 +215,26 @@ public class WebController {
             , @RequestParam(value = "studentId", required = false, defaultValue = "0") long studentId
             , @RequestParam(value = "jobId", required = false, defaultValue = "0") long jobId, Model model) {
         log.info("***** viewApplication2 *****");
-        model.addAttribute("searchMethod", searchMethod);
         boolean noList = false;
         switch (searchMethod) {
             case 2 -> {
-                List<Major> majors = majorRepository.getAllMajors();
-                model.addAttribute("majorList", majors);
-                model.addAttribute("noList", noList);
-                List<ViewApplicationForm> applications = applicationRepo.getApplicationByMajorId(major);
-                model.addAttribute("majorResults", applications);
+                model.addAttribute("majorList", majorRepository.getAllMajors());
+                model.addAttribute("majorResults", applicationRepo.getApplicationByMajorId(major));
             }
             case 3 -> {
-                List<Student> students = studentRepo.getAllStudents();
-                model.addAttribute("studentList", students);
-                model.addAttribute("noList", noList);
-                List<ViewApplicationForm> applications = applicationRepo.getApplicationByStudentId(studentId);
-                model.addAttribute("studentResults", applications);
+                model.addAttribute("studentList", studentRepo.getAllStudents());
+                model.addAttribute("studentResults", applicationRepo.getApplicationByStudentId(studentId));
             }
             case 4 -> {
-                List<Job> jobs = jobRepo.getAllJobs();
-                model.addAttribute("jobList", jobs);
-                model.addAttribute("noList", noList);
-                List<ViewApplicationForm> applications = applicationRepo.getApplicationByJobId(jobId);
-                model.addAttribute("jobResults", applications);
+                model.addAttribute("jobList", jobRepo.getAllJobs());
+                model.addAttribute("jobResults", applicationRepo.getApplicationByJobId(jobId));
             }
             default -> {
                 noList = true;
-                model.addAttribute("noList", noList);
             }
         }
+        model.addAttribute("noList", noList);
+        model.addAttribute("searchMethod", searchMethod);
         model.addAttribute("viewName", "view_applications");
         return "view_applications";
     }
