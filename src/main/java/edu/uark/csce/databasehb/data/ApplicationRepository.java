cc1ab0620acefd5c616ffbc4716c79fe431ed06e
Bailey Grimes <baileyjgrimes@gmail.com>
2023-04-18 13:10:30 -0500

    Update viewApplication queries

--- src/main/java/edu/uark/csce/databasehb/data/ApplicationRepository.java
+++ src/main/java/edu/uark/csce/databasehb/data/ApplicationRepository.java
@@ -23,8 +23,10 @@ public class ApplicationRepository {
 
     public List<ViewApplicationForm> getAllApplications() {
         List<ViewApplicationForm> applications = new ArrayList<>(template.query("SELECT s.student_name, j.company_name, j.salary, m.major_desc " +
-                        "FROM applications a, students s, jobs j, majors m, job_majors jm " +
-                        "WHERE a.student_id = s.student_id AND s.major_id = m.major_id AND a.job_id = j.job_id AND j.job_id = jm.job_id AND jm.major_id = m.major_id",
+                        "FROM applications a " +
+                        "INNER JOIN students s ON a.student_id = s.student_id " +
+                        "INNER JOIN majors m ON s.major_id = m.major_id " +
+                        "INNER JOIN jobs j ON a.job_id = j.job_id",
                 (rs, rowNum) -> new ViewApplicationForm(rs.getString("student_name"), rs.getString("company_name"),
                         rs.getLong("salary"), rs.getString("major_desc"))));
         return applications.isEmpty() ? null : applications;
@@ -32,8 +34,12 @@ public class ApplicationRepository {
 
     public List<ViewApplicationForm> getApplicationByMajorId(int majorId) {
         List<ViewApplicationForm> applications = new ArrayList<>(template.query("SELECT s.student_name, j.company_name, j.salary, m.major_desc " +
-                        "FROM applications a, students s, jobs j, majors m, job_majors jm " +
-                        "WHERE a.student_id = s.student_id AND s.major_id = m.major_id AND a.job_id = j.job_id AND j.job_id = jm.job_id AND jm.major_id = m.major_id AND m.major_id = ?",
+                        "FROM applications a " +
+                        "INNER JOIN students s ON a.student_id = s.student_id " +
+                        "INNER JOIN jobs j ON a.job_id = j.job_id " +
+                        "INNER JOIN job_majors jm on j.job_id = jm.job_id " +
+                        "INNER JOIN majors m ON m.major_id = jm.major_id " +
+                        "WHERE jm.major_id = ?",
                 (rs, rowNum) -> new ViewApplicationForm(rs.getString("student_name"), rs.getString("company_name"),
                         rs.getLong("salary"), rs.getString("major_desc")), majorId));
         return applications.isEmpty() ? null : applications;
